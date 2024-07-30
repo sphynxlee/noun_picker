4ca252fb23a009684015e926115752e917f94b7b
haidun.lhd <kaiton.ri@gmail.com>
2024-07-30 15:00:47 -0300

    chore: Add launch configuration for Python debugger and backend gitignore

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+# ref: https://spacy.io/
+
+# pip install -U spacy
+# python -m spacy download en_core_web_sm
+import spacy
+from flask import Flask, request, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+# Load English tokenizer, tagger, parser and NER
+nlp = spacy.load("en_core_web_sm")
+
+# test for backend server
+@app.route('/')
+def hello():
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return f'Hello, Spacy! Current time is {current_time}.'
+
+@app.route('/process', methods=['POST'])
+def process_text():
+    data = request.json
+    text = data.get('text')
+
+    doc = nlp(text)
+    noun_phrases = [chunk.text for chunk in doc.noun_chunks]
+    print('Noun Phrases:', noun_phrases)
+
+    return jsonify(noun_phrases=noun_phrases)
+
+if __name__ == '__main__':
+    app.run()
+
